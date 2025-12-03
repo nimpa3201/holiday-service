6713e0d2f6aeebf31e353cd1519deab29ce3460d
gwongilnam <nimpa3201@naver.com>
2025-12-03 21:06:05 +0900

    chore: 원격 main 브랜치 변경사항 로컬에 병합

---
+++ src/main/java/com/planitsquare/holidayservice/HolidayserviceApplication.java
@@ -0,0 +1,13 @@
+package com.planitsquare.holidayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planitsquare/holidayservice/HolidayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planitsquare.holidayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
