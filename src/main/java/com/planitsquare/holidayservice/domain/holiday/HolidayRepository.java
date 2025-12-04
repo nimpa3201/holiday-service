227cb15c73e01f0bf157eeaa5b91433ffecf391c
gwongilnam <nimpa3201@naver.com>
2025-12-04 19:12:36 +0900

    feat: Country/Holiday Repository 추가

---
+++ src/main/java/com/planitsquare/holidayservice/domain/holiday/HolidayRepository.java
@@ -0,0 +1,7 @@
+package com.planitsquare.holidayservice.domain.holiday;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface HolidayRepository extends JpaRepository<Holiday, Long> {}
