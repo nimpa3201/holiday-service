c359b32821f8bd7bd771fed83bf51decb5ab8a5a
gwongilnam <nimpa3201@naver.com>
2025-12-04 18:52:43 +0900

    feat: Country, Holiday 엔티티 및 연관관계 매핑 추가

---
+++ src/main/java/com/planitsquare/holidayservice/domain/country/Country.java
@@ -0,0 +1,34 @@
+package com.planitsquare.holidayservice.domain.country;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false,length = 10)
+    private String code;
+
+    @Column(nullable = false,length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private boolean used = true;
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public void markSupported(boolean used) {
+        this.used= used;
+    }
+}
---
+++ src/main/java/com/planitsquare/holidayservice/domain/holiday/Holiday.java
@@ -0,0 +1,82 @@
+package com.planitsquare.holidayservice.domain.holiday;
+
+import com.planitsquare.holidayservice.domain.country.Country;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Holiday {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false, length = 200)
+    private String localName;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(nullable = false)
+    private boolean fixed;
+
+    @Column(nullable = false)
+    private boolean globalHoliday;
+
+    private Integer launchYear;
+
+    @Column(length = 200)
+    private String types;
+
+    public static Holiday create(
+        Country country,
+        LocalDate date,
+        String localName,
+        String name,
+        boolean fixed,
+        boolean globalHoliday,
+        Integer launchYear,
+        List<String> typeList
+    ) {
+        Holiday h = new Holiday();
+        h.country = country;
+        h.date = date;
+        h.year = date != null ? date.getYear() : null;
+        h.localName = localName;
+        h.name = name;
+        h.fixed = fixed;
+        h.globalHoliday = globalHoliday;
+        h.launchYear = launchYear;
+        h.setTypesFromList(typeList);
+        return h;
+    }
+
+    public void setTypesFromList(List<String> typeList) {
+        if (typeList == null || typeList.isEmpty()) {
+            this.types = null;
+        } else {
+            this.types = String.join(",", typeList);
+        }
+    }
+}
+
+
+
+
+
